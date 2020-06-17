a0d4b1d863bb5442b9726e52b528c01a74e5381e
amayacausey <amayacausey@gmail.com>
2020-06-17 08:18:52 -0700

    first commit

---
+++ src/Bugs.java
@@ -0,0 +1,12 @@
+public class Bugs {
+    public static void main(String args[]) {
+        int sum = 0;
+
+        for (int i = 8; i > 0; i = i- 2) {
+            sum = sum + i;
+            System.out.println("Sum=" +sum);
+        }
+
+
+    }
+}
